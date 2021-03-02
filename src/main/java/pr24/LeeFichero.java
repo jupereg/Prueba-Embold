8e67e9be4f0654eb67dfb25cc1d4e91dfdd07fbb
Juan <jupereg@unirioja.es>
2021-03-02 18:47:26 +0100

    terminado

--- src/main/java/pr24/LeeFichero.java
+++ src/main/java/pr24/LeeFichero.java
@@ -10,76 +10,78 @@ import java.util.Properties;
 
 public class LeeFichero {
 	public static float salarioMedio() {
-		int contLineas=0;
-		float salario;
-		float suma=0;
-		int i=0;
-		boolean camposVacios;
-		 try {
-			 BufferedReader f=new BufferedReader(new FileReader("src/main/java/pr24/datos.txt"));
-			 //DataInputStream f = new DataInputStream(
-			 //new FileInputStream(<nombre-fichero>));
-			 String l = f.readLine();
-			 String[] parts=l.split("\\|");
-			 while (l != null) {
-			 // Procesar la línea
-				 camposVacios=false;
-				 contLineas++;
-				 if(parts.length==5) {
-					 while(i<5 && !camposVacios) {
-						 if(parts[i].isEmpty())
-							 camposVacios=true;
-						 i++;
-					 }
-					 if(i==4) {
-					 salario=Float.parseFloat(parts[3]);
-					 suma=suma+salario;
-					 }
-				  }	 
-				 l = f.readLine();
-			 	}
-			 f.close();
-			 } 
-		 catch (IOException ex) {
-			 ex.printStackTrace();
-		 }
-		 catch(NumberFormatException e) {
-			 e.printStackTrace();
-		 }
-		 	
-		 return suma/contLineas;
-	}
-	
-	
-	public static List<Empleado> creaEmpleados(String ciudad){
-		List<Empleado> list=new ArrayList<Empleado>();
-		int i=0;
+		int contLineas = 0;// saber el número de líneas leidas
+		float salario;// donde se almacenará el salario de cada empleado
+		float suma = 0;// para ir sumando los salarios
+		int i = 0;
 		boolean camposVacios;
 		try {
-			 BufferedReader f=new BufferedReader(new FileReader("src/main/java/pr24/datos.txt"));
-			 //DataInputStream f = new DataInputStream(
-			 //new FileInputStream("src/main/java/pr24/datos.txt"));
-			 String l = f.readLine();
-			 String[] parts=l.split("\\|");
-			 while (l != null) {
-				 // Procesar la línea
-				 camposVacios=false;
-				 if(parts.length==5) {
-					 while(i<5 && !camposVacios) {
-						 if(parts[i].isEmpty())
-							 camposVacios=true;
-						 i++;
-					 }
-					 if(i==4 && ciudad=="Zaragoza") {
-					 list.add(new Empleado(parts[0],parts[1]));
-					 }
-				  }	
-				 l = f.readLine();
-			 }
-			 f.close();
-			 } catch (IOException ex) {
-			 ex.printStackTrace();
-			 }
+			BufferedReader f = new BufferedReader(new FileReader("src/main/java/pr24/datos.txt"));
+			// DataInputStream f = new DataInputStream(
+			// new FileInputStream(<nombre-fichero>));
+			String l = f.readLine();
+			while (l != null) {
+				// Procesar la línea
+				String[] parts = l.split("\\|");//Se tiene que hacer para cada líne
+				camposVacios = false;
+				i=0;
+				if (parts.length == 5) {// tiene que tener 5 campos
+					while (i < 5 && !camposVacios) {
+						if (parts[i].trim().isEmpty())// con que haya una vacía ya no nos vale
+							camposVacios = true;
+						i++;
+						
+					}
+					if (!camposVacios) {// los 5 campos están completos
+						contLineas++;
+						salario = Float.parseFloat(parts[3]);
+						suma = suma + salario;
+						
+					}
+					
+				}
+				l = f.readLine();//leer la siguiente línea
+			}
+			f.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+
+		return suma / contLineas;
+	}
+
+	public static List<Empleado> creaEmpleados(String ciudad) {
+		List<Empleado> list = new ArrayList<Empleado>();
+		int i = 0;
+		boolean camposVacios;
+		try {
+			BufferedReader f = new BufferedReader(new FileReader("src/main/java/pr24/datos.txt"));
+			// DataInputStream f = new DataInputStream(
+			// new FileInputStream("src/main/java/pr24/datos.txt"));
+			String l = f.readLine();
+			while (l != null) {
+				// Procesar la línea
+				String[] parts = l.split("\\|");//Se tiene que hacer para cada línea
+				camposVacios = false;
+				if (parts.length == 5) {// tiene que tener 5 campos
+					while (i < 5 && !camposVacios) {
+						if (parts[i].trim().isEmpty())// con que haya una vacía ya no nos vale
+							camposVacios = true;
+						i++;
+					}
+					if (!camposVacios && parts[4].equalsIgnoreCase(ciudad)) {// los 5 campos están completos y la ciudad es la misma que la pasada por parámetro
+						list.add(new Empleado(parts[0], parts[1]));//añade un nuevo empleado a list con el nombre y apellido de la línea
+					}
+				}
+				l = f.readLine();//leer la siguiente línea
+			}
+			f.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 		return list;
 	}
+
 }
